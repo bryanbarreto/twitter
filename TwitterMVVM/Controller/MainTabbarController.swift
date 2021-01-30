5ba5717f951bd196cb922059028a8824e0f5dabb
Bryan Barreto <bryan.barreto@icloud.com>
2021-01-30 00:31:11 -0300

    creating project structure folders and main tabbar

---
+++ TwitterMVVM/Controller/MainTabbarController.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabbarController.swift
+//  TwitterMVVM
+//
+//  Created by Bryan Barreto on 30/01/21.
+//
+
+import UIKit
+
+class MainTabbarController: UITabBarController {
+    override func viewDidLoad() {
+        self.view.backgroundColor = .blue
+    }
+}
